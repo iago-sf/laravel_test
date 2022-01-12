1cdd7c5d786398b6f21202710ff17f903f3d2fd3
iago <iaguerreles@gmail.com>
2022-01-12 18:39:14 +0000

    A40 Ejercicio 3

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.app')
 @section('content')
+<div class="text-center">
 <h1>Community</h1>
+
+<div class="m-3">
+@foreach ($links as $link)
+  <li>{{$link->title}}</li>
+@endforeach
+</div>
+{{$links->links()}}
+</div>
 @stop
